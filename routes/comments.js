bb62a51e42e155ea304798ff739138b53899ce44
Hayden <haydenworden@outlook.com>
2018-06-10 15:57:50 +0000

    Middleware folder added

--- routes/comments.js
+++ routes/comments.js
@@ -2,13 +2,14 @@ var express = require("express"),
 // Able to acsess :id
 router = express.Router({mergeParams:true}),
 Product	= require("../models/product"),
+middleware = require("../middleware"),
 Comment	= require("../models/comments");
 
 // ==========
 // NEW COMMENT FORM
 // ==========
 
-router.get("/new", isLoggedIn, function(req, res){
+router.get("/new", middleware.isLoggedIn, function(req, res){
   Product.findById(req.params.id, function(err, product){
     if(err){
       console.log(err);
@@ -22,7 +23,7 @@ router.get("/new", isLoggedIn, function(req, res){
 // POST NEW COMMENT
 // ==========
 
-router.post("/", isLoggedIn,function(req ,res){
+router.post("/", middleware.isLoggedIn,function(req ,res){
   Product.findById(req.params.id, function (err, product) {
     if(err){
       console.log(err);
@@ -50,7 +51,7 @@ router.post("/", isLoggedIn,function(req ,res){
 // EDIT COMMENT
 // ==========
 
-router.get("/:comment_id/edit",isLoggedIn, function(req, res){
+router.get("/:comment_id/edit", middleware.checkCommentOwnership, function(req, res){
   Product.findById(req.params.id, function(err, product){
     if(err){
       console.log(err);
@@ -70,7 +71,7 @@ router.get("/:comment_id/edit",isLoggedIn, function(req, res){
 // UPDATE COMMENT
 // ==========
 
-router.put("/:comment_id/edit",isLoggedIn, function(req, res){
+router.put("/:comment_id/edit", middleware.checkCommentOwnership, function(req, res){
   Product.findById(req.params.id, function(err, product){
     if(err){
       console.log(err);
@@ -90,7 +91,7 @@ router.put("/:comment_id/edit",isLoggedIn, function(req, res){
 // DELETE COMMENT
 // ==========
 
-router.delete("/:comment_id",isLoggedIn, function (req, res) {
+router.delete("/:comment_id", middleware.checkCommentOwnership, function (req, res) {
   Product.findById(req.params.id, function(err, product) {
       if(err){
         console.log(err);
@@ -106,11 +107,39 @@ router.delete("/:comment_id",isLoggedIn, function (req, res) {
   });
 });
 
-function isLoggedIn(req, res, next){
-  if(req.isAuthenticated()){
-    return next();
-  }
-  res.redirect("/login");
-}
+// // Checks if user can edit comment
+// function checkCommentOwnershipEdit(req, res, next){
+// 	if(req.isAuthenticated()){
+		
+// 		Product.findById(req.params.id, function(err, product){
+// 			if(err){
+// 				console.log(err);
+// 			}else{
+// 				Comment.findById(req.params.comment_id, function(err, comment){
+// 					if(err){
+// 						console.log(err);
+// 					}else{
+// 						if(req.user._id.equals(comment.author.id)){
+// 							next();
+							
+// 						}else{
+// 							res.redirect("/blogs/"+req.params.id);
+// 						}
+// 					}
+// 				});
+// 			}
+// 		});
+		
+// 	}else{
+// 		res.redirect("/login");
+// 	}
+// }
+
+// function isLoggedIn(req, res, next){
+//   if(req.isAuthenticated()){
+//     return next();
+//   }
+//   res.redirect("/login");
+// }
 
 module.exports = router;
