bbe313a399204808569b71a0ed726883e5b59f55
Hayden <haydenworden@outlook.com>
2018-06-17 10:19:11 +0000

    Search js finished

--- public/js/search.js
+++ public/js/search.js
@@ -1,23 +1,45 @@
-let filterInput = document.querySelector("#filterInput");
-let product = document.querySelectorAll("#productBox");
-let title = document.querySelectorAll("#productTitle");
+let filterInput = document.querySelector("#filterInput"),
+ product = document.querySelectorAll("#productBox"),
+ title = document.querySelectorAll("#productTitle"),
+ user = document.querySelectorAll("#userTitle"),
+ productTab = document.querySelector('#product-tab'),
+ userTab = document.querySelector('#user-tab');
+ 
+ filterInput.addEventListener("keyup", productSearch);
+ productTab.addEventListener('click', function(){
+  filterInput.addEventListener("keyup", productSearch);
+ });
+ 
+ userTab.addEventListener('click', function(){
+  
+  filterInput.addEventListener("keyup", userSearch);
+ });
+ 
 
-product.forEach(function(item){
-    item.style.display='none';
-});
-
-filterInput.addEventListener("keyup", productSearch);
 
 function productSearch(){
-    // Get value of input
+  let inputValue = filterInput.value.toUpperCase();
+  // Get value of input
+  // Compare to product name
+  for(let i = 0; i < product.length;i++){
+    let a = title[i].innerHTML;
+      if(a.toUpperCase().indexOf(inputValue) > -1){
+        product[i].style.display='flex';
+      }else{
+    product[i].style.display='none';
+    }
+  }
+}
+function userSearch(){
     let inputValue = filterInput.value.toUpperCase();
+    // Get value of input
     // Compare to product name
-    for(let i = 0; i < product.length;i++){
-        let a = title[i].innerHTML;
+    for(let i = 0; i < user.length;i++){
+        let a = user[i].innerHTML;
         if(a.toUpperCase().indexOf(inputValue) > -1){
-            product[i].style.display='flex';
+            user[i].style.display='block';
         }else{
-            product[i].style.display='none';
+            user[i].style.display='none';
         }
     }
 }
